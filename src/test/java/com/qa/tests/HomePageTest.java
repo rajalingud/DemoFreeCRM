9c67eccbeaf161d6bc6a8934f7a0f02245f463e8
Rajalingu Dasari <rdasari@Rajalingu-Dasari.local>
2018-02-08 12:04:52 +0530

    HomePageTest Update

---
+++ src/test/java/com/qa/tests/HomePageTest.java
@@ -0,0 +1,10 @@
+package com.qa.tests;
+
+public class HomePageTest {
+	
+	public void HomePage()
+	{
+		System.out.println("This is HompePageTest");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -0,0 +1,15 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	public void logintest()
+	{
+		System.out.println("This is login test");
+	}
+
+}
+
+// Users/rdasari/Documents/Eclipse Workspace/DemoGitRepoMaven
